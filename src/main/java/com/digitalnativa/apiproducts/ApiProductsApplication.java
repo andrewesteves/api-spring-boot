27c35fe85269739b916d695424d9c8b139e0d428
Andrew Esteves <esteves.andrew@gmail.com>
2018-10-20 11:26:24 -0300

    Application structured start

---
+++ src/main/java/com/digitalnativa/apiproducts/ApiProductsApplication.java
@@ -0,0 +1,12 @@
+package com.digitalnativa.apiproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProductsApplication.class, args);
+	}
+}
