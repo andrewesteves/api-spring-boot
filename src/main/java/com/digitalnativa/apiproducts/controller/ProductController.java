eb6e664ea27b17b651f295a034005474136c7614
Andrew Esteves <esteves.andrew@gmail.com>
2018-10-20 12:24:17 -0300

    Show the product by its id

--- src/main/java/com/digitalnativa/apiproducts/controller/ProductController.java
+++ src/main/java/com/digitalnativa/apiproducts/controller/ProductController.java
@@ -5,11 +5,13 @@ import com.digitalnativa.apiproducts.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -22,5 +24,14 @@ public class ProductController {
     public ResponseEntity<List<Product>> index() {
         List<Product> products = productRespository.findAll();
         return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "{id}", method = RequestMethod.GET)
+    public ResponseEntity<Product> show(@PathVariable String id) {
+        Optional<Product> product = productRespository.findById(id);
+        if(!product.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(product.get(), HttpStatus.OK);
     }
 }
